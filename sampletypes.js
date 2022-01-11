b3d6a6e946f510a1e80c355a4a59dacf8524421e
aannaanntt <anantmishra2307@gmail.com>
2022-01-11 17:59:07 +0530

    first commit

---
+++ sampletypes.js
@@ -0,0 +1,6 @@
+var found = true;
+var grade = 88.0;
+var marks = 78;
+var firstName = 'Anant';
+var lastName = "Mishra";
+console.log("Hello ".concat(firstName, "  ").concat(lastName));
---
+++ sampletypes.ts
@@ -0,0 +1,6 @@
+let found:boolean = true;
+let grade:number =88.0
+let marks : number = 78 
+let firstName: string = 'Anant';
+let lastName : string = "Mishra";
+console.log(`Hello ${firstName}  ${lastName}`)
